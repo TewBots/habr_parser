afafd0d8ed8d6e20d489c228e3536226a7c22a36
TewBots <115368570+TewBots@users.noreply.github.com>
2022-10-29 10:48:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,11 @@ headers = {
 url = 'https://habr.com/ru/all/'
 url_json = 'https://habr.com/kek/v2/articles/695972/?fl=ru&hl=ru'
 
-API = '5676696817:AAFvIdIZ-f4-B6W1SvQSSjD0_W_tRT2WXLA'
+API = 'YOUR_TELEGRAM_TOKEN'
 
 proxies = {
-    "https": "http://landickp:h6vXdu56vd@193.178.134.161:50100"
+    "https": "PROXY"
 }
 
 url_habr = 'https://habr.com/ru/all/'
 
-start_buttons = ['Хакер', 'Аминистрирование', 'Кодинг', 'Приватность', 'Трюки', 'Последнии пять', 'Новые']
